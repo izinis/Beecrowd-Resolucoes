fd432d68686d11adc474865cdc852fba147c701b
unknown <izinis.caroline@gmail.com>
2022-09-16 08:56:10 -0300

    Conclusão dos execícios 1008 e 1009

---
+++ 1008.py
@@ -0,0 +1,6 @@
+F = int(input())
+H = int(input())
+V = float(input())
+
+print("NUMBER =",F)
+print("SALARY = U$ {:.2F}".format(V*H))
---
+++ 1009.py
@@ -0,0 +1,7 @@
+NOME = input()
+SALARIO = float(input())
+VENDAS = float(input())
+
+TOTAL = SALARIO + (VENDAS*0.15)
+
+print("TOTAL = R$ {:.2f}".format(TOTAL))
